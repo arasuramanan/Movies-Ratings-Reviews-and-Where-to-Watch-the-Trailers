04cec5202da88e59bc9093862222af141d76022b
arasuramanan <arasuaudi@gmail.com>
2023-11-19 15:09:30 +0530

    eight commit

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
+
+
 export function Home() {
-  return <h1>Welcome to Trailer and Movies Application 🎉🎶✨🎁</h1>;
-
-
-  
-  
+  return (
+    <>
+      <h1>Welcome to Trailer and Movies Application 🎉🎶✨🎁</h1>
+      <p>In the movie selection click on the i icon to watch trailers</p>
+    </>
+  );
 }
+
